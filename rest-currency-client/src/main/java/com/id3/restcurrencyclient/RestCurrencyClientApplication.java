eef14d6b84dde09916cdddd9f22dcace4e351af5
RamazanAkdag <akdagramazan586@gmail.com>
2024-07-05 11:08:56 +0300

    rest currency client initialized

---
+++ rest-currency-client/src/main/java/com/id3/restcurrencyclient/RestCurrencyClientApplication.java
@@ -0,0 +1,13 @@
+package com.id3.restcurrencyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCurrencyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCurrencyClientApplication.class, args);
+	}
+
+}
--- src/main/java/com/id3/currencyservice/ftpclient/FtpClientImpl.java
+++ src/main/java/com/id3/currencyservice/ftpclient/FtpClientImpl.java
@@ -1,5 +1,6 @@
 package com.id3.currencyservice.ftpclient;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -8,14 +9,15 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @Service
+@Slf4j
 public class FtpClientImpl extends FtpClient{
     public void uploadFile(File file, String remotePath) {
         try (FileInputStream fis = new FileInputStream(file)) {
             boolean done = getFtpClient().storeFile(remotePath, fis);
             if (done) {
-                System.out.println("The file is uploaded successfully.");
+                log.info("The file is uploaded successfully.");
             } else {
-                System.out.println("Failed to upload the file.");
+                log.error("Failed to upload the file.");
             }
 
         } catch (FileNotFoundException e) {
